1433d0b4cbe20d03b74fbdc1367ed4200242017a
Abdelkader Lounis <46940200+abdoutony@users.noreply.github.com>
2022-07-21 15:57:36 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// let btn = document.querySelector("#small-screen-nav-btn");
+// console.log(btn);
+// btn.addEventListener("click", () => {
+//   let todisplay = document.getElementById("navbar-content");
+//   console.log(todisplay);
+//   todisplay.style.display = "block !important";
+// });
